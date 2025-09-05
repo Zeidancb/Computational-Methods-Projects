5eaf1361063d2513d59fcf535619909537fae6ec
zeidancb <zeidancb@vt.edu>
2025-09-05 15:27:59 -0400

    Solved the Isosceles Triangle Problem

--- HW2/Chap2Prob6/PIFROMAREA.cpp
+++ HW2/Chap2Prob6/PIFROMAREA.cpp
@@ -5,8 +5,7 @@
 #include <limits>
 #include <numbers>
 // 
-// threePi is a small program to obtain pi in three
-// different ways:
+// threePi is a small program to obtain pi in one way
 //
 int main () {
 
@@ -20,8 +19,12 @@ int main () {
   unsigned int nsides=6;
   while (nsides < std::numeric_limits<int>::max()) {
 
-    double lside = abs(x1-x0);
-    double approx= nsides*lside/2.0;
+    // Area can be written as A_t = 0.5 * | x1*(y2 - y3) + x2*(y3 - y1) + x3*(y1 - y2) |
+    // Note that the base of the isosceles triangle makes up a single side
+    // of the polygon, and hence A = n * A_t where A_t denotes the area 
+    // of the single triangle
+    double area = 0.5 * std::fabs(std::imag(std::conj(x0) * x1));
+    double approx= nsides * area;
     std::cout << "Sides " << nsides << "; approx=" << std::setprecision(16) << approx << std::endl;
 
     x1=(x1+x0)/std::abs(x1+x0);
